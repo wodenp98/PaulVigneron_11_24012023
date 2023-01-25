10e884f1c1651a8835f9ba4432d24241d10a3384
Paul VIGNERON <paulvigneron4698@gmail.Com>
2023-01-25 15:06:00 +0100

    routes and some files

---
+++ kasa/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Lodging from "./pages/Lodging";
+import Error from "./pages/Error";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/lodging" element={<Lodging />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
