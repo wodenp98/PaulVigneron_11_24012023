b6fb937a9aceb6dcd7701858ae774ea32435d4cd
Paul VIGNERON <paulvigneron4698@gmail.Com>
2023-02-06 17:37:25 +0100

    error on the lodging id page

--- kasa/src/pages/Lodging/Lodging.jsx
+++ kasa/src/pages/Lodging/Lodging.jsx
@@ -4,58 +4,62 @@ import Carousel from "../../components/Carousel/Carousel";
 import Tags from "../../components/Tags/Tags";
 import Rating from "../../components/Rating/Rating";
 import Dropdown from "../../components/Dropdown/Dropdown";
+import Error from "../Error/Error";
 
 const Lodging = () => {
   const { id } = useParams();
 
   const dataLodging = Datas.find((data) => data.id === id);
 
-  const {
-    description,
-    equipments,
-    host,
-    location,
-    pictures,
-    rating,
-    tags,
-    title,
-  } = dataLodging;
+  if (!dataLodging) {
+    return <Error />;
+  } else {
+    const {
+      description,
+      equipments,
+      host,
+      location,
+      pictures,
+      rating,
+      tags,
+      title,
+    } = dataLodging;
 
-  let fullName = host.name.split(" ");
-  let firstName = fullName[0];
-  let lastName = fullName[1];
+    let fullName = host.name.split(" ");
+    let firstName = fullName[0];
+    let lastName = fullName[1];
+    return (
+      <main className="lodgingPage">
+        <Carousel imgCarousel={pictures} />
 
-  return (
-    <main className="lodgingPage">
-      <Carousel imgCarousel={pictures} />
+        <div className="presentation">
+          <h2>{title}</h2>
+          <p>{location}</p>
+          <Tags tags={tags} />
 
-      <div className="presentation">
-        <h2>{title}</h2>
-        <p>{location}</p>
-        <Tags tags={tags} />
-
-        <div className="landlord">
-          <div className="landlordInformation">
-            <div className="landlordName">
-              <p>{firstName}</p>
-              <p>{lastName}</p>
+          <div className="landlord">
+            <div className="landlordInformation">
+              <div className="landlordName">
+                <p>{firstName}</p>
+                <p>{lastName}</p>
+              </div>
+              <div className="landlordImage">
+                <img src={host.picture} alt={`propriétaire: ${host.name}`} />
+              </div>
             </div>
-            <div className="landlordImage">
-              <img src={host.picture} alt={`propriétaire: ${host.name}`} />
+            <div className="rating">
+              <Rating rating={rating} />
             </div>
-          </div>
-          <div className="rating">
-            <Rating rating={rating} />
           </div>
         </div>
-      </div>
 
-      <div className="description">
-        <Dropdown title={"Description"} text={description} />
-        <Dropdown title={"Équipements"} text={equipments} />
-      </div>
-    </main>
-  );
+        <div className="description">
+          <Dropdown title={"Description"} text={description} />
+          <Dropdown title={"Équipements"} text={equipments} />
+        </div>
+      </main>
+    );
+  }
 };
 
 export default Lodging;
